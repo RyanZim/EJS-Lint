1e05fc19ed7fa7124c344a1ef6acc9d4fc7f7513
Daniel Kneip <daniel.kneip@pagnos.com>
2020-10-08 17:58:45 +0200

    fix: Remove error introduced from testing

--- cli.js
+++ cli.js
@@ -16,7 +16,7 @@ const { argv } = require('yargs')
     describe: 'Allow old (pre-EJS v3) preprocessor-style includes',
     type: 'boolean',
   })
-  .option('allow-await', {
+  .option('await', {
     describe: 'Allow usage of await in template',
     type: 'boolean',
   });
@@ -28,7 +28,7 @@ const ejsLint = require('./index.js');
 const opts = {
   delimiter: argv.delimiter,
   preprocessorInclude: argv['preprocessor-include'],
-  await: argv['allow-await'],
+  await: argv.await,
 };
 read(glob(argv._))
   .then((res) => {
